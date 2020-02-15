1b534ad9e06ca040f7cdfa1cdfc571c4012971ee
Nik Steel <nikolassteel@gmail.com>
2020-02-15 18:03:12 -0500

    add page routing

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { HomeRoutingModule } from './home-routing.module';
+import { RouterModule } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    RouterModule.forChild(
+      [
+        { path: 'home', component: HomeComponent },
+        { path: '**', redirectTo: 'home'}
+      ]
+    )
+  ]
+})
+export class HomeModule { }
